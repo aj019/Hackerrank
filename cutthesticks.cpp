a87e9f8b73399cf67dbef75d59a22d2fb127af31
aj019 <junagupta19@gmail.com>
2016-06-17 10:03:48 +0530

    Fixed errors

--- cutthesticks.cpp
+++ cutthesticks.cpp
@@ -3,28 +3,45 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <climits>
 using namespace std;
 
+ void cut(int a[],int n,int smallest){
+    int i;
+    for(i=0;i<n;i++){
+        a[i] = a[i] - smallest;
+    }
+}
+
+ int find_small(int a[],int n){
+    int small = INT_MAX;
+
+    for(int i=0;i<n;i++){
+        if(a[i] <small && a[i] > 0)
+            small = a[i];
+    }
+
+    return small;
+}
+
 int main(){
-    int n,i,j,temp;
+    int n,count,small;
     cin >> n;
-    vector<int> arr(n);
+    int arr[n];
     for(int arr_i = 0;arr_i < n;arr_i++){
        cin >> arr[arr_i];
     }
-    for(i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
-           if(arr[j]<arr[i]){
-              temp = arr[i];
-              arr[i] = arr[j];
-              arr[j] = temp;
-           }
-
+    count = n;
+    while(count > 0){
+        cout<<count<<"\n";
+        small = find_small(arr,n);
+        cut(arr,n,small);
+        count = 0;
+        for(int j=0;j<n;j++){
+            if(arr[j] >0)
+                count++;
         }
     }
 
-    for(i=0;i<n;i++){
-        cout<<arr[i];
-    }
     return 0;
 }
