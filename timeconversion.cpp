e7f89edb52a994ba567e6ca35210951fa968cda1
aj019 <junagupta19@gmail.com>
2016-06-17 09:05:40 +0530

    Fixed errors

--- timeconversion.cpp
+++ timeconversion.cpp
@@ -4,21 +4,23 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <iomanip>
 using namespace std;
 
 
-int main() {
-   long hh,mm,ss;
-    char ch[] ="AM";
+
+int main(){
+    int hh,mm,ss;
+    char ch[2];
     char s;
     cin>>hh>>s>>mm>>s>>ss>>ch;
 
     if(strcmp(ch,"AM")==0){
-        cout<<hh<<":"<<mm<<":"<<ss;
+        cout<<setfill('0')<<setw(2)<<hh<<":"<<setfill('0')<<setw(2)<<mm<<":"<<setfill('0')<<setw(2)<<ss;
     }
     else if(strcmp(ch,"PM")==0){
         hh+=12;
-        cout<<hh<<":"<<mm<<":"<<ss;
+        cout<<setfill('0')<<setw(2)<<hh<<":"<<setfill('0')<<setw(2)<<mm<<":"<<setfill('0')<<setw(2)<<ss;
     }
     return 0;
 }
